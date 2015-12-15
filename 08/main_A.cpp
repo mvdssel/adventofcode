8a6122fb229c12656a3ab20f430ab9ac6f349efe
Waldo Plasmatics <maartenvandessel@hotmail.com>
2015-12-15 15:26:47 +0100

    Day 8

---
+++ 08/main_A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <map>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    const char *input = "input.txt";
+    fstream ifs(input, fstream::in);
+    string line;
+
+    map<regex *, string> replaces;
+    replaces.insert(make_pair(new regex("^\"(.*)\"$"), "$1"));
+    replaces.insert(make_pair(new regex("\\\\\\\\"), "\\"));
+    replaces.insert(make_pair(new regex("\\\\\""), "\""));
+    replaces.insert(make_pair(new regex("\\\\x[0-9a-f]{2}"), "@"));
+
+    int total = 0;
+
+    while(getline(ifs, line)) {
+        total += line.size();
+
+        // cout << setw(50) << line << " " << line.size() << " ";
+
+        for_each(replaces.begin(), replaces.end(), [&line] (pair<regex *, string> replace) {
+                line = regex_replace(line, *(replace.first), replace.second);
+        });
+
+        // cout << setw(50) << line << " " << line.size() << " ";
+
+        total -= line.size();
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
---
+++ 08/main_B.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <map>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    const char *input = "input.txt";
+    fstream ifs(input, fstream::in);
+    string line;
+
+    map<regex *, string> replaces;
+    replaces.insert(make_pair(new regex("\\\\"), "\\\\"));
+    replaces.insert(make_pair(new regex("\""), "\\\""));
+    replaces.insert(make_pair(new regex("^(.*)$"), "\"$1\""));
+
+    int total = 0;
+
+    while(getline(ifs, line)) {
+        total -= line.size();
+
+        // cout << setw(50) << line << " " << line.size() << " ";
+
+        for_each(replaces.begin(), replaces.end(), [&line] (pair<regex *, string> replace) {
+                line = regex_replace(line, *(replace.first), replace.second);
+        });
+
+        // cout << line.size() << " " << line << endl;
+
+        total += line.size();
+    }
+
+    cout << total << endl;
+
+    return 0;
+}
